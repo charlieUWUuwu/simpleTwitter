cb2940b9d19a568f5d088441892543c826022b05
charlie <123charlie200@gmail.com>
2025-04-23 00:05:30 +0800

    feat: apply JWT authentication to user routes

--- backend/app/api/users_routes.py
+++ backend/app/api/users_routes.py
@@ -1,11 +1,19 @@
 from flask import Blueprint, request, jsonify
+import jwt
+from datetime import datetime, timedelta, timezone
 from app.services.users_service import UsersService
 from app.utils.validator import Validator
 from app.exceptions import UserNotFoundError
+from app.utils.auth_decorators import login_required, admin_required
+import os
 
 users_bp = Blueprint('users', __name__)
+SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+ALGORITHM = os.getenv("JWT_ALGORITHM")
 
 @users_bp.route('/', methods=['GET'])
+@login_required
+@admin_required
 def get_all_users():
     try:
         users = UsersService.get_all_users()
@@ -32,7 +40,12 @@ def login():
 
     try:
         user = UsersService.login(email, password)
-        return jsonify({"success": True, "msg": "Login successfully", "data": user}), 200
+        user["exp"] = datetime.now(timezone.utc) + timedelta(seconds=20) # 新增 exp 欄位（10秒後過期）
+
+        # 登入成功後產生 JWT Token
+        token = jwt.encode(user, SECRET_KEY, algorithm=ALGORITHM)
+
+        return jsonify({"success": True, "msg": "Login successfully", "token": token}), 200
     except UserNotFoundError as e:
         return jsonify({"success": False, "msg": str(e)}), 404
     except ValueError as e:
@@ -67,6 +80,7 @@ def create_user():
         return jsonify({"success": False, "msg": "Internal server error"}), 500
 
 @users_bp.route('/<int:user_id>', methods=['GET'])
+@login_required
 def get_user_by_id(user_id):
     try:
         user = UsersService.get_user_by_id(user_id)
@@ -79,6 +93,8 @@ def get_user_by_id(user_id):
         return jsonify({"success": False, "msg": "Internal server error"}), 500
 
 @users_bp.route('/<int:user_id>', methods=['DELETE'])
+@login_required
+@admin_required
 def delete_user(user_id):
     try:
         user = UsersService.delete_user(user_id)
